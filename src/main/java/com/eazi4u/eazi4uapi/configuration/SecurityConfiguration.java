bcd9ffc217ba852902781bebf0eab1444fbb204b
Michael <mihendricks1@gmail.com>
2020-01-01 11:28:09 +0200

    feat: Cors Configuration

--- src/main/java/com/eazi4u/eazi4uapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/eazi4u/eazi4uapi/configuration/SecurityConfiguration.java
@@ -2,14 +2,19 @@ package com.eazi4u.eazi4uapi.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 
 @Configuration
@@ -24,18 +29,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/roles/**", "/users/**")
-                .hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and().formLogin()
-                .and().oauth2Login().successHandler(oauth2authSuccessHandler);
+        http.cors()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/roles/**", "/users/**")
+            .hasRole("ADMIN")
+//                .antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+            .and().formLogin()
+            .and().oauth2Login().successHandler(oauth2authSuccessHandler);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
